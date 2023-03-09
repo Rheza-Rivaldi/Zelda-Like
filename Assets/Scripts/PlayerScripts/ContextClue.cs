1552029e4e20c07218b25a3b047e5bc8cbc7b22f
Rheza-Rivaldi <101277746+Rheza-Rivaldi@users.noreply.github.com>
2023-03-09 22:38:37 +0700

    Add Context Clue
    Added Context Clue signal and some clean up

---
+++ Assets/Scripts/PlayerScripts/ContextClue.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextClue : MonoBehaviour
+{
+    public GameObject contextClue;
+    public bool contextActive = false;
+
+    public void ChangeContext(){
+        contextActive = !contextActive;
+        contextClue.SetActive(contextActive);
+    }
+}
